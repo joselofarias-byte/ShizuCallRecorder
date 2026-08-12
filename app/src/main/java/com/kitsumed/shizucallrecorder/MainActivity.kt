1caea199a954dc3fc4aa648f894ccdc1e10008ac
joselofarias-byte <289238002+joselofarias-byte@users.noreply.github.com>
2026-08-12 09:02:39 -0300

    fix(ui): enable edge-to-edge content

--- app/src/main/java/com/kitsumed/shizucallrecorder/MainActivity.kt
+++ app/src/main/java/com/kitsumed/shizucallrecorder/MainActivity.kt
@@ -10,6 +10,7 @@ package com.kitsumed.shizucallrecorder
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -22,6 +23,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             AppNavigationScreen()
         }
